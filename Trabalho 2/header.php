5dca6098933abc2b7910787a613d40a5aa0b3eba
DanielGarciaMorais <141430385+DanielGarciaMorais@users.noreply.github.com>
2024-05-20 21:09:11 -0300

    Primeiro envio do projeto de PHP (Positivo)
    Primeiro envio do projeto de PHP na faculdade Positivo, professora Juliana.
    Nesse projeto a ideia principal é criar uma interação com banco de dados, através de CRUDs, sendo o primeiro envio apenas o basico.

---
+++ Trabalho 2/header.php
@@ -0,0 +1,28 @@
+<div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-transparent px-3 m-3 mx-4 ms-5">
+        <a class="navbar-brand mb-1 fontFalloutMenu" href="#">Planejador Pós-Apocalíptico</a>
+        <div>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link fontFalloutMenu" href="/Trabalho 2/index.php">Pagina Principal</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fontFalloutMenu" href="#">Assentamentos</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle fontFalloutMenu" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Colonos</a>
+                    <div class="dropdown-menu bg-success bg-opacity-75" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item fontFalloutMenu" href="/Trabalho 2/cadastrarColono.php">Cadastrar</a>
+                        <a class="dropdown-item fontFalloutMenu" href="/Trabalho 2/visualizarColono.php">Visualizar</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fontFalloutMenu" href="#">Recursos</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
---
+++ Trabalho 2/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Planejador Pós-Apocalíptico</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+    <?php
+    include 'header.php';
+    ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-1"></div>
+            <div class="col-10">
+                <div>
+                    <h1 class="text-center mt-3">Pagina principal</h1>
+                </div>
+                <div>
+                    <p class="mt-4 fs-3 text-justify">Assuma o controle e lidere a reconstrução dos assentamentos do Commonwealth. Com o Planejador Pós-Apocalíptico, você tem o poder de gerenciar recursos, coordenar atividades e proteger seus sobreviventes. Prepare-se para enfrentar desafios e construir um novo futuro. Seu destino começa agora.</p>
+                </div>
+            </div>
+            <div class="col-1"></div>
+        </div>
+    </div>
+    <?php
+    include 'footer.php';
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
